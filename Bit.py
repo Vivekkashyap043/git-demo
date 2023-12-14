84d08535acce88fa5ebe33526b11fbdd682d4a54
vivek <vivekkashyap043@gmail.com>
2023-12-14 11:50:24 +0530

    bit is added

---
+++ Bit.py
@@ -0,0 +1,10 @@
+t = int(input())
+count = 0
+for i in range(t):
+    s = input()
+    if s == "++X" or s == "X++":
+        count += 1
+    else:
+        count -= 1    
+        
+print(count)    
